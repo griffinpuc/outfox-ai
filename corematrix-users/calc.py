89417f17987d63a12726569338d1356ce453c14f
Griffin Puc <griffinpuc@Outlook.com>
2021-10-06 16:11:34 -0400

    commit

---
+++ corematrix-users/calc.py
@@ -0,0 +1,185 @@
+#
+# -----------------------------------------------------
+#                       calc.py
+#           Holds all calculation functions
+# -----------------------------------------------------
+#
+# LAST UPDATED: 12 SEPTEMBER 2021
+# UPDATED BY: GCP
+#
+
+import pandas as pd
+import funcs
+
+# RETURN DATA FRAME:
+# returns a data frame from a csv file
+def returnDf(csvLink):
+    return pd.read_csv(csvLink,encoding='cp1252')
+
+# GENERATE BOOLS:
+# generates a boolean tables based on categorys
+def generateBools(csvLink, outputName):
+    df = pd.read_csv(csvLink)
+
+    # create empty list
+    tag_lists = []
+
+    # loop through
+    for string in df["tags"]:
+        subtag_list = []
+        for substring in str(string).split(","):
+            subtag_list.append(substring.strip())
+        tag_lists.append(subtag_list)
+        
+    df["tags"] = tag_lists
+
+    unique_tags = get_all_unique(df["tags"])
+    df_bool = create_boolean_df(unique_tags, df["tags"])
+    df_bool.shape
+    df_bool.info()
+    df_bool = df_bool.set_index(df["class_name"])
+
+    print(df_bool.info())
+
+    print('\n==========================================\n')
+    print('      BOOL TABLE GENERATION COMPLETE')
+    print('\n==========================================\n')
+    return df_bool.to_csv()
+
+# CORRELATION:
+# returns correlation value between 0 and 1
+def correlation(df, tag_a, tag_b):
+    
+    # Get all rows where a == True
+    a = df[df[tag_a]]
+    # Get all rows where b == True
+    b = df[df[tag_b]]
+    
+    # Find all rows where a AND b == True
+    a_and_b = df[ (df[tag_a]) & (df[tag_b]) ]
+    
+    # Find all rows where a == True AND b != True
+    a_not_b = df[ (df[tag_a]) & ~ (df[tag_b]) ]
+    # Find all rows where b == True AND a != True
+    b_not_a = df[ (df[tag_b]) & ~ (df[tag_a]) ]
+    
+    # Calculate the number of possitive and possible outcomes using the shape attribute
+    possible_outcomes = a_and_b.shape[0] + a_not_b.shape[0] + b_not_a.shape[0] # shape[0] returns the number of rows
+    positive_outcomes = a_and_b.shape[0]
+    
+    # Calculate the final correlation coefficient
+    r = positive_outcomes / possible_outcomes
+    
+    return r
+
+# CORRELATION WITH EVERY TAG:
+# generates correlation values for a tag against every tag
+def correlate_with_every_tag(df, tag_a, dict_mode = True): 
+    
+    unique_tags = list(df.columns)
+    unique_tags.pop(0)
+    # In dict_mode, the results are stored in a dict, which is good for analyzing one tag
+    # However, in order to transform the data into a df later, we need a list output
+    if dict_mode:
+        # Loop through every tag and store the correlation in the dict
+        correlation_dict = {}
+        for tag_b in unique_tags:
+            correlation_dict[tag_b] = correlation(df, tag_a, tag_b)
+        return correlation_dict
+    else:
+        # Loop through every tag and store the correlation in a list
+        correlation_list = []
+        for tag_b in unique_tags:
+            correlation_list.append(correlation(df, tag_a, tag_b))
+        return correlation_list
+
+# CREATE BOOLEAN DF:
+# generate a boolean data frame
+def create_boolean_df(unique_tags, group_taglists):
+    # Create new df with a column for every tag
+    boolean_df = pd.DataFrame(columns = unique_tags)
+    boolean_df.shape
+    
+    # Create an empty dict
+    data_dict = {}
+    
+    # Loop through the columns (tags) in the boolean_df and add them to the dict
+    for col in boolean_df:
+        data_dict[col] = []
+        
+        # Loop through the taglists in the old dataframe
+        for taglist in group_taglists:
+            
+            # Check if the column (tag) is in the tracks taglist. If yes append True else append False
+            data_dict[col].append(col in taglist)
+    
+    # Use the boolean lists as values for the boolean_df's columns
+    for col in boolean_df:
+        boolean_df[col] = data_dict[col]
+        
+    return boolean_df
+
+# GET ALL UNIQUE:
+# gets all unique tags in a dataframe of groups
+def get_all_unique(dataframe_of_lists):
+    # Create empty list
+    unique_tags = []
+    
+    # Loop through the dataframe rows (lists) and each item inside
+    for row in dataframe_of_lists:
+        for item in row:
+            
+            # Add item to all_tags if it's not already in it
+            if item not in unique_tags:
+                unique_tags.append(item)
+                
+    return unique_tags
+
+# FIND CORRELATIONS:
+# finds correlations in dataframe from list of tags
+def find_correlations(df, tag):
+    
+    # Setup empty list
+    correlations = []
+    columns = []
+
+    # Loop through all column at the row with the tag as its index
+    try:
+        for i, corr in enumerate(df.loc[tag,:]):
+            # Find the column
+            col = df.columns[i]
+            
+            # Append the correlation to the list
+            correlations.append(corr)
+            columns.append(col)
+    except KeyError:
+        print("INVALID CATEGORY TAG")
+        pass
+
+    # Create a df out of the lists
+    results_df = pd.DataFrame({"tag" : columns,
+                              "correlation" : correlations})
+    
+    return results_df
+
+# FIND HIGHEST CORRELATION:
+# finds highest correlation tags from a tag
+def find_highest_correlations(corr_df, num_of_values):
+    
+    # Sort the input df
+    corr_df_sorted = corr_df.sort_values(by = ["correlation"], ascending = False)
+    
+    # Extract the relevant correlations
+    corr_df_sliced = corr_df_sorted.iloc[1:num_of_values+1]
+    
+    return corr_df_sliced
+
+# BASE LEVEL GET RECOMMENDATION:
+# takes dataframe, tag, and rec count
+def get_recommendations(df, tag, num_of_recommendations):
+    
+    corr_df = find_correlations(df, tag)
+    
+    recommendations_df = find_highest_correlations(corr_df, num_of_recommendations)
+    
+    print("Recommendations:", list(recommendations_df["tag"]))
---
+++ corematrix-users/main.py
@@ -0,0 +1,110 @@
+#
+# -----------------------------------------------------
+# OUTFOX-AI: CM3 (CORRELATIONAL-MATRIX-MODEL-MANAGER)
+#                  main.py
+#          Main base file for function
+# -----------------------------------------------------
+#
+# LAST UPDATED: 12 SEPTEMBER 2021
+# UPDATED BY: GCP
+#
+
+import calc
+import funcs
+import sys
+import os
+import csv
+import pandas as pd
+from tqdm import tqdm
+
+# VALUES:
+# probably shouldnt change
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
+csvLink1 = ROOT_DIR+"\\csv\\raw_data.csv"
+csvLink2 = ROOT_DIR+"\\csv\\group_tags_bool.csv"
+csvLink3 = ROOT_DIR+"\\csv\\group_tags_corr_matrix.csv"
+
+# GENERATE BOOL MATRIX:
+# generates a csv file with bool values corresponding to each category tag
+def generateBoolMatrix():
+    try:
+        os.remove(csvLink2)
+    except OSError:
+        pass
+    
+    funcs.saveFile(csvLink2, calc.generateBools(csvLink1,  "tags_bool"))
+    print(('EXPORT TO: {path}').format(path=csvLink2))
+
+# CALCULATE CORRELATIONAL MATRIX
+# generates a correlational matrix for each category tag
+def calculateCorrelationMatrix():
+    try:
+        os.remove(csvLink3)
+    except OSError:
+        pass
+    df = calc.returnDf(csvLink2)
+    unique_tags = list(df.columns)
+    unique_tags.pop(0)
+
+    correlation_matrix_dict = {}
+
+    for tag_a in tqdm(unique_tags):
+        correlation_matrix_dict[tag_a] = calc.correlate_with_every_tag(df, tag_a, dict_mode = False)
+
+    df_corr_matrix = pd.DataFrame(correlation_matrix_dict)
+    df_corr_matrix.shape
+    df_corr_matrix.head()
+    df_corr_matrix["index"] = unique_tags
+    df_corr_matrix = df_corr_matrix.set_index("index")
+    funcs.saveFile(csvLink3, df_corr_matrix.to_csv())
+
+# CALCULATE RECOMMENDATIONS
+# calculates a list of recommended categories based on a category input
+def calculateRecommendations(param, resultNo):
+    df = pd.read_csv(csvLink3, index_col = "index",encoding='cp1252')
+    calc.get_recommendations(df, param, resultNo)
+
+def listTags():
+    df = calc.returnDf(csvLink2)
+    taglist = ""
+    for string in list(df.columns):
+        taglist += (string+', ')
+    print(taglist)
+
+
+# THROW ERROR FUNCTION
+# throw cmd error/help function
+def throwCmdError():
+    print('\nOUTFOX-AI CM3:\n')
+    print('    CMD                           |     DESCRIPTION')
+    print('------------------------------------------------------------------------------------------')
+    print('main.py gbt                       |  GENERATE BOOL TABLE')
+    print('main.py gcm                       |  GENERATE CORRELATIONAL MATRIX')
+    print('main.py rec <cat tag> <# to gen>  |  GENERATE RECOMMENDATIONS')
+    print('main.py tags                      |  LIST ALL TAGS')
+    print('Suggested workflow:')
+    print('Man. import raw_data.csv > gbt > gcm\n')
+
+# MAIN FUNCTION
+# takes args and does some stuff
+if __name__ == "__main__":
+
+    if((sys.argv[1]) == "help"):
+        throwCmdError()
+    else:
+        option = (sys.argv[1])
+
+        if(option == "gbt"):
+            generateBoolMatrix()
+        elif(option == "gcm"):
+            calculateCorrelationMatrix()
+        elif(option == "rec"):
+            term = (sys.argv[2]).upper()
+            num = int(sys.argv[3])
+            calculateRecommendations(term, num)
+        elif(option == "tags"):
+            listTags()
+        elif(option == "fmcsv"):
+            formatCSV()
+        else:
+            print('Unrecognized command.')
