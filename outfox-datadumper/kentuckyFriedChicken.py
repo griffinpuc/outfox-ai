dcb0a9f17264ea1d257e93df74afda2e718f175f
Griffin Puc <griffinpuc@Outlook.com>
2021-11-17 17:56:50 -0500

    stuff

--- outfox-datadumper/kentuckyFriedChicken.py
+++ outfox-datadumper/kentuckyFriedChicken.py
@@ -1,5 +1,8 @@
 import wikipedia
+import csvdata
 import sys
+import os
+from pathlib import Path
 
 #
 #THROW ERROR FUNCTION
@@ -9,25 +12,44 @@ def throwCmdError():
     print('main.py <total entries> <total groups per user> <link to csv>\n')
 
 def kfcMagicSeasoning(amount):
-    
+    for i in range(0, amount):
+        while True:
+
+            csvrow = csvdata.getRow()
+            tag = csvrow[1].split(',')[0]
+            path = "data/resources/"+tag
+            if not os.path.exists(path):
+                break
+        
+
+        print("TAG "+str(i)+": " + tag)
+        Path(path).mkdir(parents=True, exist_ok=True)
+
+        kfcMagicRecipe(tag)
 
 #ANOTHER GOOD THING YES
-def kfcMagicRecipe(amount):
-    for i in range(0, amount):
+def kfcMagicRecipe(tag):
+    print(str(wikipedia.search(tag)))
 
-        print(str(wikipedia.search("chemistry")))
+    #random = wikipedia.random(1)
+    #esult = wikipedia.page(random)
 
-        random = wikipedia.random(1)
-        result = wikipedia.page(random)
+    #name = result.title+".txt"
+    #content = result.content
 
-        name = result.title+".txt"
-        content = result.content
+    for string in wikipedia.search(tag):
+        try:
+            page = wikipedia.page(string)
 
-        saveChickenBucket(name, content)
+            name = page.title
+            content = page.content
+            tag = tag
+        finally:
+            saveChickenBucket(name, content, tag)
 
-
-def saveChickenBucket(fileName, txtBody):
-    f = open("data/resources/"+fileName, "w", encoding="utf-8")
+#SAVE THE FILE
+def saveChickenBucket(fileName, txtBody, tag):
+    f = open("data/resources/"+tag+"/"+fileName+".txt", "w", encoding="utf-8")
     f.writelines(txtBody)
     print('Saved 1 file.')
 
@@ -37,4 +59,7 @@ if __name__ == "__main__":
         throwCmdError()
     else:
         resourceTotal = int(sys.argv[1])
-        kfcMagicRecipe(resourceTotal)
+
+        csvdata.parseCSV("data\dataset1.csv")
+
+        kfcMagicSeasoning(resourceTotal)
